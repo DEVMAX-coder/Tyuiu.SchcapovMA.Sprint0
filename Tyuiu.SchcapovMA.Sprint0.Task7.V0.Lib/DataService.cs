977b1153c510e5e986072b4562d4c0d33dca7f45
Максим Щапов <maksimsapov0456@gmail.com>
2024-09-20 03:33:19 +0500

    Task7

---
+++ Tyuiu.SchcapovMA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.SchcapovMA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArry = new int[5];
+            for (var i=0; i<resultArry.Length; i++)
+            {
+                resultArry[i] = numOne[i] + numTwo[i];
+            }
+            return resultArry;
+        }   
+    }
+}
